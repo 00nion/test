d224a75bde7f5dd9fd39dbcc89cc94f753a2ecd8
onion <134505813+00nion@users.noreply.github.com>
2025-03-24 23:28:24 +0700

    Save next.config.mjs.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // Ignore TypeScript errors during build
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Ignore ESLint errors during build
     ignoreDuringBuilds: true,
   },
 };
