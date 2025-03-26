1537b8e8a52257df194cd905b330f7c99a35b472
onion <134505813+00nion@users.noreply.github.com>
2025-03-27 00:30:45 +0700

    Save route.ts.

--- app/api/script/frweerobux/route.ts
+++ app/api/script/frweerobux/route.ts
@@ -68,7 +68,7 @@ loadstring(decompressed)()
     return NextResponse.json(
       {
         version: "0",
-        message: "stop tryna skidding ppls script it's disgusting",
+        message: "stop tryna skidding ppl's script it's disgusting",
         status: "unauthorized",
         token: sessionToken,
       },
