7e1e1176edd63c18038aa7268aba0d38e38e05e3
onion <134505813+00nion@users.noreply.github.com>
2025-03-29 19:28:11 +0700

    Save page.tsx.

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
       <div className="relative z-10 w-[500px] max-w-[90vw] my-10">
         <div className="rounded-xl border border-[rgb(60,60,60)] hover:border-[rgb(72,138,182)] bg-[rgba(30,30,30,0.65)] backdrop-blur-sm p-8 relative overflow-hidden transition-all duration-300 hover:shadow-[0_0_15px_rgba(72,138,182,0.3)]">
           {/* Noise overlay with adjusted transparency */}
-          <div className="absolute inset-0 bg-noise opacity-[0.9] mix-blend-soft-light"></div>
+          <div className="absolute inset-0 bg-noise opacity-[0.1] mix-blend-soft-light"></div>
 
           {/* Content */}
           <div className="relative z-10">
